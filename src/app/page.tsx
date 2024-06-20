f760098d89e855213cd2eaa0684518d2590164fd
max <77105535+weltmx@users.noreply.github.com>
2024-06-20 21:35:28 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,123 @@
-import Image from "next/image";
+import localFont from 'next/font/local'
+
+const comicsans = localFont({ src: '../fonts/comicsans.ttf' })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className='w-full relative overflow-hidden'>
+      <div
+        className='bg-repeat bg-center relative px-9 py-20'
+        style={{ backgroundImage: 'url(/images/night-sky-tranquilo.gif)' }}
+      >
+        <div className='mx-auto max-w-md'>
+          <div className='py-20'>
+            <img
+              alt=''
+              className='w-[400px] mx-auto'
+              width={250}
+              height={20}
+              src='/images/planebar.gif'
             />
-          </a>
+          </div>
+
+          <h1
+            className='text-7xl py-12 text-center font-bold italic -rotate-2'
+            style={{
+              filter:
+                'drop-shadow(30px 10px 4px #4444dd)  drop-shadow(50px 20px 4px #4444dd80)  drop-shadow(70px 30px 4px #4444dd80)',
+            }}
+          >
+            CLAYZONE
+          </h1>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div
+        className='bg-repeat bg-center px-9 py-9 h-[280px] relative'
+        style={{ backgroundImage: 'url(/images/space.png)' }}
+      >
+        <img
+          alt=''
+          className='w-[160px] object-contain absolute bottom-12 left-[50%] translate-x-[-200px]'
+          width={106}
+          height={106}
+          src='/images/pla_new.gif'
+        />
+        <img
+          alt=''
+          className='w-[220px] object-contain absolute bottom-[200px] right-[40%] translate-x-[200px]'
+          width={106}
+          height={106}
+          src='/images/plane7-c.gif'
+        />
+        <img
+          alt=''
+          className='w-[500px] object-contain absolute top-[-100px] left-[3%] translate-x-[-200px]'
+          width={106}
+          height={106}
+          src='/images/32.gif'
+        />
+
+        <img
+          alt=''
+          className='w-[300px] object-contain absolute top-[100px] right-[20%] translate-x-[200px]'
+          width={106}
+          height={106}
+          src='/images/18.gif'
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div
+        className='bg-repeat bg-center px-9 py-9 h-[420px] relative'
+        style={{ backgroundImage: 'url(/images/sky.png)' }}
+      >
+        <img
+          alt=''
+          className='w-[160px] object-contain absolute top-[20px] left-[10%]'
+          width={106}
+          height={106}
+          src='/images/robot.gif'
+        />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <img
+          alt=''
+          className='w-[230px] object-contain absolute bottom-[20px] left-[20%]'
+          width={106}
+          height={106}
+          src='/images/seikorobot.gif'
+        />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <img
+          alt=''
+          className='w-[200px] object-contain absolute top-[-60px] right-[6%]'
+          width={106}
+          height={106}
+          src='/images/plane.gif'
+        />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <img
+          alt=''
+          className='w-[200px] object-contain absolute bottom-[10%] right-[10%]'
+          width={106}
+          height={106}
+          src='/images/plane_1_.gif'
+        />
+
+        <h2
+          className={`${comicsans.className} text-5xl absolute left-6 right-6 top-[40%] text-center text-pink-500`}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          I like planes & mechanical engineering
+        </h2>
+      </div>
+
+      <div
+        className='h-[120px] bg-center bg-contain bg-black invert'
+        style={{ backgroundImage: 'url(/images/plane3d.gif)' }}
+      ></div>
+
+      <div className='py-[100px]'>
+        <img alt='' className='w-[30px] mx-auto' width={106} height={106} src='/images/52.gif' />
       </div>
     </main>
-  );
+  )
 }
